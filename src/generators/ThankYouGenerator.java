e8baa0485c06ff3a6e0a14fb2add75e957401d23
Liarra <m.e.tigra@gmail.com>
2013-06-04 19:18:03 +0400

    Merge branch 'iframes' into HEAD
    Conflicts:
    	src/generators/ThankYouGenerator.java

--- src/generators/ThankYouGenerator.java
+++ src/generators/ThankYouGenerator.java
@@ -8,19 +8,21 @@ package generators;
  */
 public class ThankYouGenerator {
 
-    public String generateThankYou(String backgroundImage,String adImage){
+    public String generateThankYou(String backgroundImage, String adImage) {
         String ret;
 
         String template =
-                "<link href=\"css/thankyou.css\" type=\"text/css\" rel=\"stylesheet\"/>\n" +
-
-                "<div id=\"ThankYouBar\" style=\"background-image:url(%s);\">\n" +
-                "    <div id=\"ThankYouString\">Спасибо!</div>\n" +
-                "</div>\n" +
-                "\n" +
-                "<div id=\"AdPics\">%s</div>" +
-                        "<a href=# onclick='parent.postToWall(1)'>123</a>";
-        ret=String.format(template,backgroundImage,adImage);
+                ""  +
+                        "<div id=\"ThankYouBar\" style=\"background-image:url(%s);\">\n" +
+                        " <link href=\"css/thankyou.css\" type=\"text/css\" rel=\"stylesheet\"/>"+
+                        ""+
+                        "    <div id=\"ThankYouString\">Спасибо!</div>\n" +
+                        "</div>\n" +
+                        "<div id='SponsorsText'>Наши спонсоры делают вашу помощь реальной:</div>" +
+                        "\n" +
+                        "<div id='SomeKindStuffHere'> 1%s</div>" +
+                        "<div id='TellFriends' ><a href='#'onclick='postToWall(1);'><i class=\"icon-bullhorn\"></i> Расскажи друзьям!</a></div>";
+        ret = String.format(template, backgroundImage, adImage);
         return ret;
     }
 }
